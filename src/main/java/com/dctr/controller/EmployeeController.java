d227cb7a1a0a036271c480d516f055b43f418b13
g0shk3 <56648912+g0shk3@users.noreply.github.com>
2020-08-01 10:44:47 +0300

    Update EmployeeController.java

--- src/main/java/com/dctr/controller/EmployeeController.java
+++ src/main/java/com/dctr/controller/EmployeeController.java
@@ -14,7 +14,7 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-@CrossOrigin(origins = "http://localhost:3000")
+@CrossOrigin(origins = "https://g0shk3.netlify.app")
 @RestController
 @RequestMapping("api/v1/")
 public class EmployeeController {
